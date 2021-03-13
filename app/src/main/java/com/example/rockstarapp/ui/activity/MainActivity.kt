e7b5d51f02625115be0c9a1e93a6f70d73f5dd9e
YMD45 <icecross125@gmail.com>
2021-03-13 20:58:07 +0100

    Ajout des 3 fragments + Gestion de la navbar

---
+++ app/src/main/java/com/example/rockstarapp/ui/activity/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.rockstarapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.rockstarapp.R
+import com.example.rockstarapp.ui.fragment.BookmarkFragment
+import com.example.rockstarapp.ui.fragment.ProfilFragment
+import com.example.rockstarapp.ui.fragment.RockstarFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private val rockstarFragment = RockstarFragment();
+    private val profilFragment = ProfilFragment();
+    private val bookmarkFragment = BookmarkFragment();
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        replaceFragment(rockstarFragment)
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.bookmarks -> replaceFragment(bookmarkFragment)
+                R.id.profil -> replaceFragment(profilFragment)
+                R.id.rockstar -> replaceFragment(rockstarFragment)
+            }
+            true
+        }
+    }
+
+    /*
+    * Replace the current fragment of the MainActivity,
+    *
+    * @param fragment fragment to show
+    * return nothing
+    */
+    private fun replaceFragment (fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,fragment)
+        transaction.commit()
+    }
+}
