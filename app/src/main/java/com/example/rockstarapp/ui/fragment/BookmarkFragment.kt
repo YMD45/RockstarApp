add861510d4876c3a83188c1b5d456bf516f162d
YMD45 <icecross125@gmail.com>
2021-03-14 01:32:23 +0100

    Interface Bookmark fonctionnelle

--- app/src/main/java/com/example/rockstarapp/ui/fragment/BookmarkFragment.kt
+++ app/src/main/java/com/example/rockstarapp/ui/fragment/BookmarkFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import com.example.rockstarapp.R
+import com.example.rockstarapp.database.AppDatabase
+import com.example.rockstarapp.model.Rockstar
+import com.example.rockstarapp.adapter.RockstarListViewAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +25,9 @@ class BookmarkFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var listViewRockstarBookmarked: ListView
+    private lateinit var listRockstarBookmared:ArrayList<Rockstar>
+    private lateinit var adapter: RockstarListViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +42,26 @@ class BookmarkFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+        val root = inflater.inflate(R.layout.fragment_bookmark, container, false)
+        listViewRockstarBookmarked = root.findViewById(R.id.list_rockstars_bookmarked)
+
+        setBookmarkedRockstar()
+
+        adapter = RockstarListViewAdapter(requireActivity(),listRockstarBookmared,true)
+        listViewRockstarBookmarked.adapter = adapter
+        return root
+    }
+
+    private fun setBookmarkedRockstar() {
+        listRockstarBookmared = ArrayList()
+        val db = AppDatabase(requireContext())
+        var bookmarkedRockstar = db.RockstarDao().findByBookmark(true)
+
+        if (bookmarkedRockstar.isNotEmpty()){
+            for(rockstar in bookmarkedRockstar) {
+                listRockstarBookmared.add(rockstar)
+            }
+        }
     }
 
     companion object {
