2f44479a1ce64499b31932693a63a1acbf3bb2de
YMD45 <icecross125@gmail.com>
2021-03-14 02:28:17 +0100

    Gestion des photos + sauvegarde fonctionnelle

--- app/src/main/java/com/example/rockstarapp/ui/fragment/ProfilFragment.kt
+++ app/src/main/java/com/example/rockstarapp/ui/fragment/ProfilFragment.kt
@@ -1,11 +1,23 @@
 package com.example.rockstarapp.ui.fragment
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import com.example.rockstarapp.R
+import com.example.rockstarapp.database.AppDatabase
+import com.example.rockstarapp.model.Profil
+import java.io.File
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,9 +30,23 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProfilFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    //Vue
+    private lateinit var saveButton:ImageButton
+    private lateinit var imageProfil:ImageView
+    private lateinit var editNameProfil:EditText
+
+    private  var profil: Profil = Profil( "" ,"")
+    private var profilExist:Boolean =false
+
+    //Gestion de l'image de profil
+    private val imgId = 70
+    private var imgUri: Uri? = null
+    private val AUTHORITY = "com.example.rockstarapp.fileprovider"
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +61,98 @@ class ProfilFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false)
+        val root = inflater.inflate(R.layout.fragment_profil, container, false)
+        val db = AppDatabase(requireContext())
+        profilExist = checkProfilExist(db)
+        if (profilExist) {
+            profil = db.ProfilDao().getProfil()
+        }
+
+        saveButton = root.findViewById(R.id.save_profil)
+        imageProfil = root.findViewById(R.id.imageProfile)
+        editNameProfil = root.findViewById(R.id.edit_name_profil)
+
+        editNameProfil.setText(profil.fullName)
+        imageProfil.setImageURI(getImgUri())
+        generateListener(profilExist,db)
+
+        return root
+    }
+
+    private fun getImgUri():Uri?{
+        if (imgUri!=null){
+            return imgUri
+        }else{
+            return Uri.parse(profil.image)
+        }
+    }
+
+    private fun checkProfilExist(db: AppDatabase):Boolean = db.ProfilDao().getProfil() != null
+
+
+    private fun generateListener(profilExist:Boolean,db:AppDatabase){
+        saveButton.setOnClickListener {
+
+            profil.fullName = getFullName(editNameProfil)
+            if(imgUri != null){
+                profil.image = imgUri.toString()
+                cleanImgFolder(profil.image)
+            }
+
+            //Si on a pas encore de profil enregistré, il faut le créer
+            if (profilExist)
+                db.ProfilDao().updateProfil(profil)
+            else
+                db.ProfilDao().insert(profil)
+        }
+
+        imageProfil.setOnClickListener(){
+
+            var imgFolder = File(requireContext().filesDir,"images");
+            if (!imgFolder.exists())
+                imgFolder.mkdir()
+
+            var image = File(imgFolder,
+                File.separator+System.currentTimeMillis() + "profil.png");
+
+            imgUri = FileProvider.getUriForFile(
+                requireContext(),
+                AUTHORITY,
+                image);
+
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivityForResult(intent,imgId)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == imgId && resultCode == RESULT_OK) {
+            profil.image = imgUri.toString()
+            imageProfil.setImageURI(imgUri)
+            saveButton.isClickable = true
+        }
+    }
+
+    private fun cleanImgFolder(imageProfilUri : String) {
+        var imgFolder = File(requireContext().filesDir,"images");
+        val segments: List<String> = imageProfilUri.split("/")
+        var imgProfilName = segments[segments.size-1]
+        val children: Array<String> = imgFolder.list()
+        for (i in children.indices) {
+            if(children[i] != imgProfilName){
+                File(imgFolder, children[i]).delete()
+            }
+        }
+    }
+
+    private fun getFullName(editTextProfil:EditText):String = if (editTextProfil.text!=null){
+        editTextProfil.text.toString()
+    } else {
+        ""
     }
 
     companion object {
